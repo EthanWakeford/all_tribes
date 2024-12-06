bc1163ae029aefdb1339bc435a973570075257f2
EthanWakeford <ethanwakeford@gmail.com>
2024-12-06 13:54:17 -0600

    more styling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,13 @@
 interface MainProps {}
 // import heroImage from '../assets/all_tribes_property.jpg';
-import heroImage from '../assets/all_tribes_blanket.jpg';
+import heroImage from '../assets/blanket_2.jpg';
 import secondImage from '../assets/all_tribes_people_close.jpg';
+import churchImage from '../assets/community_church_outside.webp';
 
 const Main: React.FC<MainProps> = ({}) => {
   return (
-    <div>
-      <div className='mt-16 px-8 mb-8 relative w-full flex flex-col'>
+    <div className='mb-32'>
+      <div className='mt-16 mb-8 px-8 relative w-full flex flex-col'>
         <h1 className='text-4xl font-bold text-center self-center font-parisienne'>
           All Tribes
         </h1>
@@ -14,30 +15,43 @@ const Main: React.FC<MainProps> = ({}) => {
           Community Church
         </h1>
         <h2 className='italic text-right'>established 1994.</h2>
-        <p>An American Baptist Churcsh USA denomination.</p>
+        <p>An American Baptist Church USA denomination.</p>
       </div>
       <div>
-        <img className='w-full h-[75vw] object-cover' src={heroImage.src}></img>
+        <img
+          className='w-full  h-[75vw] object-cover object-bottom'
+          src={heroImage.src}
+        ></img>
       </div>
       <div className='mt-16'>
-        <i>
-          "Where we are filling the spiritual needs of a diverse popilation,
-          respecting the native and non-native cultures through Jesus Christ"
-        </i>
+        <div className='px-8'>
+          <i>
+            "Where we are filling the spiritual needs of a diverse population,
+            respecting the native and non-native cultures through Jesus Christ"
+          </i>
+        </div>
         <img
           className='w-full h-[75vw] object-cover mt-8'
           src={secondImage.src}
         ></img>
       </div>
-      <div className='mt-40'>
-        <p>
-          We meet Sunday afternoons at our sister church, Community Baptist
-          Church located on Garnett just north of East 31st Street intersection
-          in East Tulsa.
-        </p>
+      <div className='mt-16'>
+        <div className='px-8'>
+          <p>
+            We meet Sunday afternoons at our sister church, Community Baptist
+            Church located on Garnett just north of East 31st Street
+            intersection in East Tulsa.
+          </p>
+        </div>
+        <img
+          className='w-full h-[75vw] object-cover mt-8'
+          src={churchImage.src}
+        ></img>
       </div>
-      <div className='mt-40'>
-        <p>All are welcome.</p>
+      <div className='mt-8'>
+        <div className='px-8'>
+          <p className='text-center'>All are welcome.</p>
+        </div>
       </div>
     </div>
   );
